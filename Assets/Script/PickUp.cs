925482cd577a6fe534227a6a8b9604130ee32ad3
Kyle Nguyen <khn200002@utdallas.edu>
2023-12-02 23:47:02 -0600

    New movement script and change to collectible.
    Create a new player script, and add a trigger on collided for collectible.

--- Assets/Script/PickUp.cs
+++ Assets/Script/PickUp.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Collider))]
 public class PickUp : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -13,15 +15,36 @@ public class PickUp : MonoBehaviour
     [SerializeField]
     [Range(0f, 100f)]
     private float _chanceToDropObjet;
-    
-    [SerializeField] private GameObject _playerObjectToCollide;
+    [SerializeField]
+    private GameObject _artToDisable;
+
+    private Collider _collider;
+    [SerializeField]
+    private float _collectibleDuration;
 
     [SerializeField] private int _damageToDeal;
     [SerializeField] private GameObject _objectToDrop;
 
-    void Start()
+    void Awake()
     {
-        
+        _collider = GetComponent<Collider>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerMovement _playerObject = other.gameObject.GetComponent<PlayerMovement>();
+        if(_playerObject != null)
+        {
+            //sequence
+        }
+
+
+    }
+
+    public IEnumerator CollectibleSequence(PlayerMovement playerMovement)
+    {
+        _collider.enabled = false;
+        _artToDisable.SetActive(false);
+        yield return new WaitForSeconds(_collectibleDuration);
     }
 
     // Update is called once per frame
