55a10daf242713cc4d359a1cc0770ab8c3d94a91
Kyle Nguyen <khn200002@utdallas.edu>
2023-12-02 23:07:35 -0600

    CollectibleType Enum

---
+++ Assets/Script/CollectibleType.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CollectibleType : MonoBehaviour
+{
+    None = 0,
+    Damage,
+    Item,
+    PowerUp,
+    PowerDown
+
+
+
+
+}
